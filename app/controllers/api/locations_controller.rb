14c1a89519824ae3fdd24d70687cd149e7b147c9
Heather You <heatherhyou@gmail.com>
2018-08-01 13:41:58 -0400

    add index and show methods to locations controller

--- app/controllers/api/locations_controller.rb
+++ app/controllers/api/locations_controller.rb
@@ -1,3 +1,26 @@
 class Api::LocationsController < ApplicationController
-    
+    before_action :set_location, only: [:show, :update, :destroy]
+
+    def index
+        render json: Location.all
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def show
+        render json: @location
+    end
+
+    private
+    def set_location
+        @location = Location.find_by(id: params[:id])
+    end
+
+    def location_params
+        params.require(:location).permit(:region, :city, :name, :address)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do 
     resources :users, only: [:edit, :update, :destroy, :show, :index]
+    resources :locations, only: [:index, :new, :create, :show]
 
     post "/signup", to: "users#signup"
     post "/login", to: "sessions#login"
