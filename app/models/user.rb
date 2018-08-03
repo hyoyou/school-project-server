d888a216c60d7c57fc7ffff42354b5a9e57579c4
pbhagyashri <bhagyashri.padmawar@gmail.com>
2018-08-03 16:07:58 -0400

    Add relationships for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ApplicationRecord
   validates :email, :username, uniqueness: true
   mount_uploader :avatar, AvatarUploader
   has_secure_password
+
+  has_many :locations through:
+
+  has_many :leaderboards
+  has_many :locations, through: :leaderboards
 end
 
 #Carrier Wave Documentation
