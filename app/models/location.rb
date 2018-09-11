3a9eadeef26d07ea5244c5d705c0f2199dfd3f5f
Heather You <heatherhyou@gmail.com>
2018-09-11 16:13:27 -0400

    Merge master branch with Leaderboard

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,12 +1,6 @@
 class Location < ApplicationRecord
     validates :name, uniqueness: true
-<<<<<<< HEAD
-
-    has_many :leaderboards
-    has_many :users, through: :leaderboards
-=======
  
     has_many :user_locations
     has_many :users, through: :user_locations
->>>>>>> Leaderboard
 end
