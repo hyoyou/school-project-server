224436e077d563219fbdc750e223852e1507252d
Heather You <heatherhyou@gmail.com>
2018-09-12 14:12:29 -0400

    Remove has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   validates :email, presence: true
   # validates :email, :username, uniqueness: true
   mount_uploader :avatar, AvatarUploader
-  has_secure_password
+  # has_secure_password
 
   has_many :user_locations
   has_many :locations, through: :user_locations
