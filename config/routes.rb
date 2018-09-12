66f96cb0d7cb6b8a7783be2364fac30e6991c0df
pbhagyashri <bhagyashri.padmawar@gmail.com>
2018-09-12 06:36:49 -0400

    Resume routes file to how it was before with devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
-
+  
   #devise_for :users
   namespace :api do
     resources :users, only: [:index, :show, :update, :destroy, :create]
     resources :locations, only: [:index, :new, :create, :show]
-    resources :user_locations, only: [:index, :show]
-
-    post "/signup", to: "users#signup"
-    post "/login", to: "sessions#login"
-    get "/logout", to: "sessions#logout"
-
+    resources :leaderboards, only: [:index, :create, :show]
+    resources :sessions, only: [:create, :destroy]
 
   end
 
