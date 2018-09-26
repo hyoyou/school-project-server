589208cd26e0f54a46a5a294dc71f91dfc4d70e9
pbhagyashri <bhagyashri.padmawar@gmail.com>
2018-09-26 04:50:23 -0400

    remove extra class declairation from auth.rb

--- lib/auth.rb
+++ lib/auth.rb
@@ -1,8 +1,6 @@
 require 'jwt'
 
 class Auth
-
-class Auth
   def self.create_token(user_object)
     payload = {user: JSON.parse(user_object.to_json)}
     token = JWT.encode(payload, "school project", "HS256")
