fc0b692d612faf54d0ecf4a4fe0b8313c2e7a793
vanitanaidu <vanita112003@gmail.com>
2018-11-06 13:12:30 -0500

    oauth authentication

--- lib/auth.rb
+++ lib/auth.rb
@@ -1,16 +1,16 @@
-require 'jwt'
-
-class Auth
-
-  # log in and sign up (create a token)
-  def self.create_token(user_object)
-    payload = {user: JSON.parse(user_object.to_json)}
-    token = JWT.encode(payload, "school project", "HS256")
-  end
-
-# authenticate user (decode a token)
-  def self.decode_token(token)
-    decode = JWT.decode(token, "school project", true, {algorithm: "HS256"})
-    return decode[0]["user"]
-  end
-end
+# require 'jwt'
+#
+# class Auth
+#
+#   # log in and sign up (create a token)
+#   def self.create_token(user_object)
+#     payload = {user: JSON.parse(user_object.to_json)}
+#     token = JWT.encode(payload, "school project", "HS256")
+#   end
+#
+# # authenticate user (decode a token)
+#   def self.decode_token(token)
+#     decode = JWT.decode(token, "school project", true, {algorithm: "HS256"})
+#     return decode[0]["user"]
+#   end
+# end
